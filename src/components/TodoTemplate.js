103f29d99e55da6dda631f44f8382113367f4048
mipi1256 <mipi1258@gmail.com>
2024-05-09 15:15:13 +0900

    할 일 삭제 기능 완료

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -59,10 +59,16 @@ const TodoTemplate = () => {
     });
   };
 
+  // 할 일 삭제 처리 함수
+  const removeTodo = (id) => {
+    //const removeTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className='TodoTemplate'>
       <TodoHeader />
-      <TodoMain todoList={todos} />
+      <TodoMain todoList={todos} remove={removeTodo} />
       <TodoInput addTodo={addTodo} />
     </div>
   );
