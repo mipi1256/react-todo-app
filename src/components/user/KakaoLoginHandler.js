19e628fe1a1f54109d2bdb5cb37c389ceda2afcd
mipi1256 <mipi1258@gmail.com>
2024-05-23 15:05:04 +0900

    카카오 로그인, 로그아웃, 프로필 사진 처리

--- src/components/user/KakaoLoginHandler.js
+++ src/components/user/KakaoLoginHandler.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { API_BASE_URL, USER } from '../../config/host-config';
+import AuthContext from '../../utils/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const KakaoLoginHandler = () => {
   console.log(
-    '사용자가 동의하면을 통해 필수 정보 동의 후 Kakao 인증 서버에서 redirect를 진행함!',
+    '사용자가 동의화면을 통해 필수 정보 동의 후 Kakao 인증 서버에서 redirect를 진행함!',
   );
+
+  const { onLogin } = useContext(AuthContext);
+  const redirection = useNavigate();
 
   const REQUEST_URL = API_BASE_URL + USER;
 
@@ -15,6 +20,13 @@ const KakaoLoginHandler = () => {
     // 컴포넌트가 렌더링 될 때, 인가 코드를 백엔드로 전송하는 fetch 요청
     const kakaoLogin = async () => {
       const res = await fetch(REQUEST_URL + '/kakaologin?code=' + code);
+
+      const { token, userName, email, role } = await res.json(); // 서버에서 온 json 읽기
+
+      // Context API를 사용하여 로그인 상태를 업데이트 합니다.
+      onLogin(token, userName, role);
+
+      redirection('/');
     };
 
     kakaoLogin();
